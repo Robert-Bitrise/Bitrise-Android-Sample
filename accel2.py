662d912edbcd0f600a460fcd48114da93bfda33b
Robert-Bitrise <67321947+Robert-Bitrise@users.noreply.github.com>
2021-12-08 02:03:59 +0100

    Update accel2.py

--- accel2.py
+++ accel2.py
@@ -12,6 +12,7 @@ auth_token = sys.argv[1]
 telnet.write(bytes("auth " + auth_token + "\r\n", 'utf-8'))
 
 accel_point=[4,3,1]
+print(accel_point)
 telnet.write(bytes("sensor set acceleration {0}:{1}:{2}\r\n".format(accel_point[0],accel_point[1],accel_point[2]), 'utf-8'))
 #result = telnet.read_until(b"OK", 10)
 #print(result.decode("utf-8"))
